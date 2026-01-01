344b4d56235ea32e249fea2c5cd68498ae6842a9
vatsakshat02 <vatsakshat02@gmail.com>
2026-01-01 18:47:01 +0530

    bug:fixed song not loading

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
-
+loadSong?(currentSongIndex)
 next.addEventListener('click',function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0]);
